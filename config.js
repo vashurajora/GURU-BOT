db4c12c14d5212fed8b7e71b0325746e6ef05332
vashurajora <118199977+vashurajora@users.noreply.github.com>
2023-11-14 11:59:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['41782220068', '➳ ⁶⁶⁶ ➢᭄࿐ᴅᴇᴍᴏɴ ˡᵒʳᵈ ꜱᴀᴛᴜʀɴ', true],
+  
   [''] 
 ] //Number of owners
 
